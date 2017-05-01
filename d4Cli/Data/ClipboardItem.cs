7de9112d6cf8e7cf3fb34e88ece4ebb079ffeaed
Jon Felix Jennemann <github@jonfelix.net>
2017-05-01 11:24:55 +0200

    Add project files.

---
+++ d4Cli/Data/ClipboardItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d4Cli.Data
+{
+    class ClipboardItem
+    {
+        private string _text;
+        private DateTime _timestamp;
+
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+            set
+            {
+                _text = value;
+            }
+        }
+
+        public DateTime Timestamp
+        {
+            get
+            {
+                return _timestamp;
+            }
+            set
+            {
+                _timestamp = value;
+            }
+        }
+    }
+}
---
+++ d4Cli/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;     
+using System.Windows.Forms;
+using d4Cli.Data
+
+namespace d4Cli
+{
+    public partial class Form1 : Form
+    {
+        List<ClipboardItem> _list = new List<ClipboardItem>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(Clipboard.ContainsText())
+            {
+                if(_list.Count == 0)
+                {
+                    AddItem();   
+                }
+                if(Clipboard.GetText() != _list[_list.Count - 1].Text)
+                {
+                    AddItem();
+                }
+            }
+            timer1.Enabled = true;
+        }
+
+        private void AddItem()
+        {
+            ClipboardItem cbi = new ClipboardItem();
+            cbi.Text = Clipboard.GetText();
+            cbi.Timestamp = DateTime.Now;
+            _list.Add(cbi);
+        }
+    }
+}
