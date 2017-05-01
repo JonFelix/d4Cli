56d948ecdf410b96b16ccb74bd4cce9dd21dd3db
Jon Felix Jennemann <github@jonfelix.net>
2017-05-01 12:49:52 +0200

    Add stuff

--- d4Cli/Form1.Designer.cs
+++ d4Cli/Form1.Designer.cs
@@ -29,41 +29,145 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LogBox = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.LogSplitter = new System.Windows.Forms.SplitContainer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuLogIsShown = new System.Windows.Forms.ToolStripMenuItem();
+            this.Browser = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.LogSplitter)).BeginInit();
+            this.LogSplitter.Panel1.SuspendLayout();
+            this.LogSplitter.Panel2.SuspendLayout();
+            this.LogSplitter.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // LogBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(593, 502);
-            this.textBox1.TabIndex = 0;
+            this.LogBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.LogBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LogBox.Location = new System.Drawing.Point(0, 0);
+            this.LogBox.Multiline = true;
+            this.LogBox.Name = "LogBox";
+            this.LogBox.ReadOnly = true;
+            this.LogBox.Size = new System.Drawing.Size(618, 260);
+            this.LogBox.TabIndex = 0;
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // LogSplitter
+            // 
+            this.LogSplitter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LogSplitter.IsSplitterFixed = true;
+            this.LogSplitter.Location = new System.Drawing.Point(0, 0);
+            this.LogSplitter.Name = "LogSplitter";
+            this.LogSplitter.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // LogSplitter.Panel1
+            // 
+            this.LogSplitter.Panel1.Controls.Add(this.Browser);
+            this.LogSplitter.Panel1.Controls.Add(this.menuStrip1);
+            // 
+            // LogSplitter.Panel2
+            // 
+            this.LogSplitter.Panel2.Controls.Add(this.LogBox);
+            this.LogSplitter.Panel2.Enabled = false;
+            this.LogSplitter.Size = new System.Drawing.Size(618, 527);
+            this.LogSplitter.SplitterDistance = 263;
+            this.LogSplitter.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(618, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuLogIsShown});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // MenuLogIsShown
+            // 
+            this.MenuLogIsShown.Checked = true;
+            this.MenuLogIsShown.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.MenuLogIsShown.Name = "MenuLogIsShown";
+            this.MenuLogIsShown.Size = new System.Drawing.Size(94, 22);
+            this.MenuLogIsShown.Text = "Log";
+            this.MenuLogIsShown.Click += new System.EventHandler(this.logToolStripMenuItem_Click);
+            // 
+            // Browser
+            // 
+            this.Browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Browser.IsWebBrowserContextMenuEnabled = false;
+            this.Browser.Location = new System.Drawing.Point(0, 24);
+            this.Browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.Browser.Name = "Browser";
+            this.Browser.ScriptErrorsSuppressed = true;
+            this.Browser.ScrollBarsEnabled = false;
+            this.Browser.Size = new System.Drawing.Size(618, 239);
+            this.Browser.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(618, 527);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LogSplitter);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.LogSplitter.Panel1.ResumeLayout(false);
+            this.LogSplitter.Panel1.PerformLayout();
+            this.LogSplitter.Panel2.ResumeLayout(false);
+            this.LogSplitter.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogSplitter)).EndInit();
+            this.LogSplitter.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox LogBox;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.SplitContainer LogSplitter;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MenuLogIsShown;
+        private System.Windows.Forms.WebBrowser Browser;
     }
 }
 
--- d4Cli/Form1.cs
+++ d4Cli/Form1.cs
@@ -1,17 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;     
+using System.Collections.Generic;
 using System.Windows.Forms;
 using d4Cli.Data;
+using System.IO;
 
 namespace d4Cli
 {
     public partial class Form1 : Form
     {
         List<ClipboardItem> _list = new List<ClipboardItem>();
+        List<string> _logMessages = new List<string>();
+        private bool _logTimestamp = true;
+        private bool _logIsShown = true;
+        private float _logSize = 0.2f;
+        private string _browserText = "";
+        private string _style= "";
 
         public Form1()
         {
             InitializeComponent();
+            MenuLogIsShown.Checked = _logIsShown;
+            ResizeLogBox();
+            Browser.DocumentText = _browserText;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -36,11 +46,69 @@ namespace d4Cli
         }
 
         private void AddItem()
-        {
+        {               
             ClipboardItem cbi = new ClipboardItem();
             cbi.Text = Clipboard.GetText();
             cbi.Timestamp = DateTime.Now;
+            Log("Add '" + cbi.Text + "' to clipboard memory.");
             _list.Add(cbi);
+            AddToBrowser(cbi);                
+        }
+
+        public void Log(string text)
+        {
+            if(_logTimestamp)
+            {
+                string timestamp = "[";
+                timestamp += DateTime.Now.Hour.ToString() + ":";
+                timestamp += DateTime.Now.Minute.ToString() + ":";
+                timestamp += DateTime.Now.Second.ToString() + ":";
+                timestamp += DateTime.Now.Millisecond.ToString() + "] ";
+                text = timestamp + text;
+            }
+            _logMessages.Add(text);
+            if(_logMessages.Count > 1)
+            {
+                text = Environment.NewLine + text;
+            }
+            LogBox.AppendText(text);
+        }
+
+        private void logToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(_logIsShown)
+            {
+                LogSplitter.Panel2.Hide();
+            }
+            else
+            {
+                LogSplitter.Panel2.Show();
+            }
+            _logIsShown = !_logIsShown;
+            MenuLogIsShown.Checked = _logIsShown;
+            ResizeLogBox();
+        } 
+
+        private void ResizeLogBox()
+        {
+            if(_logIsShown)
+            {
+                LogSplitter.SplitterDistance = (int)(Size.Height * (1 - _logSize));    
+            }                                                     
+        }
+
+        private void AddToBrowser(ClipboardItem cbi)
+        {
+            ReadStyle();
+            string preText = "<div class=\"item\"><div class=\"timestamp\">" + cbi.Timestamp.ToLocalTime().ToString() + "</div><div class=\"text\">";
+            string postText = "</div></div></br>";
+            _browserText = preText + cbi.Text + postText + _browserText;
+            Browser.DocumentText = "<!DOCTYPE html><html><head><meta http-equiv=\"X - UA - Compatible\" content =\"IE = 9\" /><style>" + _style + "</style></head><body>" + _browserText + "</body></html>";
+        }
+
+        private void ReadStyle()
+        {
+            _style = File.ReadAllText(@"style.css");
         }
     }
 }
